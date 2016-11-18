62652a9c78d3f8d877264b49487605fe2152857f
GeekRRK <669672615@qq.com>
2016-11-18 15:51:24 +0800

    Add BPAlertView

---
+++ BaseProject/ThirdParty/HZQPickerView/BPAlertView.h
@@ -0,0 +1,15 @@
+//
+//  BPAlertView.h
+//  BaseProject
+//
+//  Created by UGOMEDIA on 2016/11/18.
+//  Copyright © 2016年 UgoMedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BPAlertView : UIView
+
++ (void)showParentVC:(UIViewController *)parentVC;
+
+@end
---
+++ BaseProject/ThirdParty/HZQPickerView/BPAlertView.m
@@ -0,0 +1,17 @@
+//
+//  BPAlertView.m
+//  BaseProject
+//
+//  Created by UGOMEDIA on 2016/11/18.
+//  Copyright © 2016年 UgoMedia. All rights reserved.
+//
+
+#import "BPAlertView.h"
+
+@implementation BPAlertView
+
++ (void)showParentVC:(UIViewController *)parentVC {
+    
+}
+
+@end
