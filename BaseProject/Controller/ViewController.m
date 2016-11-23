a9b346f4580f570b0bc8411c8d04eecef6fd33fb
GeekRRK <669672615@qq.com>
2016-11-23 15:59:11 +0800

    Translusent navigationbar gradually when scroll tableview

--- BaseProject/Controller/ViewController.m
+++ BaseProject/Controller/ViewController.m
@@ -8,8 +8,19 @@
 
 #import "ViewController.h"
 #import "HZQPickerView.h"
+#import "BPTouchTableView.h"
+#import <SDCycleScrollView.h>
 
-@interface ViewController ()
+static float alpha = 1;
+static float baseMinY = -64;
+static float baseMaxY = 160 - 64;
+
+
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, SDCycleScrollViewDelegate>
+
+@property (strong, nonatomic) BPTouchTableView *tableView;
+@property (strong, nonatomic) UIView *statusView;
+@property (strong, nonatomic) UITextField *searchTextField;
 
 @end
 
@@ -18,19 +29,92 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-    btn.frame = CGRectMake(0, 0, 100, 60);
-    btn.center = self.view.center;
-    btn.backgroundColor = [UIColor blackColor];
-    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [self.view addSubview:btn];
-    [btn addTarget:self action:@selector(clickBtn) forControlEvents:UIControlEventTouchUpInside];
+    _tableView = [[BPTouchTableView alloc] initWithFrame:CGRectMake(0, -64, SCREENWIDTH, SCREENHEIGHT)];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [self.view addSubview:_tableView];
+    
+    [self setupNavBarTransparent];
+    
+    [self setupTableHeaderView];
+    
+    [self setupTitleView];
 }
 
-- (void)clickBtn {
-    [HZQPickerView showPickerViewInVC:self titles:@[@[@"1", @"2", @"3"], @[@"a", @"b", @"c"], @[@"小", @"中", @"大"]] numOfComponent:3 block:^(NSInteger index1st, NSInteger index2nd, NSInteger index3rd) {
-        NSLog(@"col1: %zd, col2: %zd, col3: %zd", index1st, index2nd, index3rd);
-    }];
+- (void)setupTitleView {
+    _searchTextField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, SCREENWIDTH - 60, 30)];
+    _searchTextField.borderStyle = UITextBorderStyleRoundedRect;
+    _searchTextField.textColor = [UIColor blackColor];
+    _searchTextField.backgroundColor = [UIColor whiteColor];
+    self.navigationItem.titleView = _searchTextField;
+}
+
+- (void)setupTableHeaderView {
+    NSArray *imageArray = @[
+                      @"http://cdn.duitang.com/uploads/item/201210/31/20121031085212_Kv4zZ.thumb.600_0.jpeg",
+                      @"http://img5.duitang.com/uploads/item/201307/09/20130709201608_t5BJF.jpeg",
+                      @"http://mg.soupingguo.com/bizhi/big/10/350/642/10350642.jpg",
+                      @"http://iphone.tgbus.com/UploadFiles/201301/20130130145233400.jpg"
+                      ];
+    
+    SDCycleScrollView *adScrollView = [[SDCycleScrollView alloc] initWithFrame:CGRectMake(0, 0, SCREENWIDTH, 160)];
+    adScrollView.delegate = self;
+    adScrollView.imageURLStringsGroup = imageArray;
+    adScrollView.autoScrollTimeInterval = 2.5;
+    _tableView.tableHeaderView = adScrollView;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 44;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *reusableCell = @"ReusableCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reusableCell];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:reusableCell];
+    }
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    cell.textLabel.text = @"iOS";
+    
+    return cell;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (scrollView.contentOffset.y <= baseMinY) {
+        scrollView.contentOffset = CGPointMake(0, baseMinY);
+    }
+    
+    float curY = scrollView.contentOffset.y - baseMinY;
+    if (curY > 0 && curY < baseMaxY) {
+        alpha = curY / baseMaxY;
+    } else if (curY <= 0) {
+        alpha = 0;
+    } else if (curY >= baseMaxY) {
+        alpha = 1;
+    }
+    
+    NSLog(@"%lf", alpha);
+    
+    self.navigationController.navigationBar.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:alpha];
+    _statusView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:alpha];
+}
+
+- (void)setupNavBarTransparent {
+    self.navigationController.navigationBar.translucent = YES;
+    //Make navigationbar transparent.
+    UIImage *navClearBkg = [UIImage imageNamed:@"TransparentPixel"];
+    [self.navigationController.navigationBar setBackgroundImage:navClearBkg forBarMetrics:UIBarMetricsDefault];
+    self.navigationController.navigationBar.shadowImage = navClearBkg;
+    
+    _statusView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREENWIDTH, 20)];
+    [self.view addSubview:_statusView];
 }
 
 @end
