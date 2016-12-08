a595ef215d1ed5a21b8b57b0b4f37c4e2fcbf995
GeekRRK <669672615@qq.com>
2016-12-08 16:48:37 +0800

    Remove unused header from BPUtil.m

--- BaseProject/Util/BPUtil.m
+++ BaseProject/Util/BPUtil.m
@@ -8,19 +8,6 @@
 //  常用方法
 
 #import "BPUtil.h"
-#import <ShareSDK/ShareSDK.h>
-#import <ShareSDKConnector/ShareSDKConnector.h>
-
-//腾讯开放平台（对应QQ和QQ空间）SDK头文件
-#import <TencentOpenAPI/TencentOAuth.h>
-#import <TencentOpenAPI/QQApiInterface.h>
-
-//微信SDK头文件
-#import "WXApi.h"
-
-//新浪微博SDK头文件
-#import "WeiboSDK.h"
-//新浪微博SDK需要在项目Build Settings中的Other Linker Flags添加"-ObjC"
 
 @implementation BPUtil
 
