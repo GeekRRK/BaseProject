92a8336e529b387add39d3005ac80cb67c5d57e8
GeekRRK <669672615@qq.com>
2017-03-14 15:10:36 +0800

    Update BPWebDetailVC.m

--- BaseProject/Controller/Common/BPWebDetailVC.m
+++ BaseProject/Controller/Common/BPWebDetailVC.m
@@ -35,4 +35,45 @@
     }];
 }
 
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    _jsContext = [_webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    _jsContext[@"xfNewsContext"] = _jsExport;
+}
+ 
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    if (!webView.isLoading) {
+        NSString *readyState = [webView stringByEvaluatingJavaScriptFromString:@"document.readyState"];
+        BOOL complete = [readyState isEqualToString:@"complete"];
+        if (complete) {
+            [self webViewDidFinishLoadCompletely];
+        } else {
+            NSString *jsString =
+            @"window.onload = function() {"
+            @"    xfNewsContext.onload();"
+            @"};"
+            @"document.onreadystatechange = function () {"
+            @"    if (document.readyState == \"complete\") {"
+            @"        xfNewsContext.documentReadyStateComplete();"
+            @"    }"
+            @"};";
+            [_webView stringByEvaluatingJavaScriptFromString:jsString];
+        }
+        NSLog(@"%@", NSStringFromSelector(_cmd));
+    }
+}
+ 
+- (void)onload {
+    [self webViewDidFinishLoadCompletely];
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+}
+ 
+- (void)documentReadyStateComplete {
+    [self webViewDidFinishLoadCompletely];
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+}
+ 
+- (void)webViewDidFinishLoadCompletely {
+    [self displayContent];
+}
+
 @end
